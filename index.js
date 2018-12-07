cffcf0bf423bb16a346d48f56ff77eb86a520636
Amenadiel <amenadiel@gmail.com>
2018-12-07 17:23:29 -0300

    publish to npm

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-const path = require('path'),
-	_ = require('lodash'),
+const path = require("path"),
+	_ = require("lodash"),
 	typefolders = [
-		'params_backbone',
-		'params_express',
-		'params_facebook_api',
-		'params_geojson',
-		'params_google_maps',
-		'params_whatwg',
+		"params_backbone",
+		"params_express",
+		"params_facebook_api",
+		"params_geojson",
+		"params_google_maps",
+		"params_google_drive",
+		"params_whatwg"
 	];
 
-
 let paramLinks = {
-	Wkt: 'https://github.com/arthur-e/Wicket',
-	Wicket: 'https://github.com/arthur-e/Wicket',
-	Promise: 'http://bluebirdjs.com/docs/api-reference.html',
+	Wkt: "https://github.com/arthur-e/Wicket",
+	Wicket: "https://github.com/arthur-e/Wicket",
+	Promise: "http://bluebirdjs.com/docs/api-reference.html",
 	HTML2Canvas: "https://html2canvas.hertzen.com/",
 	Canvg: "https://github.com/canvg/canvg",
 	jQuery: "https://api.jquery.com/jQuery/",
 	Cash: "https://github.com/kenwheeler/cash"
 };
 
-typefolders.forEach((foldername) => {
-	Object.assign(paramLinks, require(path.resolve(`${__dirname}/${foldername}`)));
+typefolders.forEach(foldername => {
+	Object.assign(
+		paramLinks,
+		require(path.resolve(`${__dirname}/${foldername}`))
+	);
 });
 
 console.log(paramLinks);
